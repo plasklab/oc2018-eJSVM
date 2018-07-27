6e52d983ba66fbec2c8999eba33574390014028c
gyaneman <gyanexus7@gmail.com>
2018-07-27 10:19:42 +0900

    Time.clock Time.get_sec_monotonic, msec, usec, nsec

--- builtin-time.c
+++ builtin-time.c
@@ -4,6 +4,8 @@
 #include "header.h"
 
 #include <unistd.h>
+#include <time.h>
+#include <sys/time.h>
 
 unsigned int to_uint(Context *ctx, JSValue v) {
   if (!is_number(v)) v = to_number(ctx, v);
@@ -35,9 +37,74 @@ BUILTIN_FUNCTION(builtin_time_udelay)
   set_a(context, JS_UNDEFINED);
 }
 
+BUILTIN_FUNCTION(builtin_time_clock)
+{
+  builtin_prologue();
+  double t = (double)clock()/(CLOCKS_PER_SEC/1000);
+  try_gc(context);
+  set_a(context, double_to_flonum(t));
+}
+
+BUILTIN_FUNCTION(builtin_time_get_sec_monotonic)
+{
+  builtin_prologue();
+  struct timespec ts;
+  if (clock_gettime(CLOCK_MONOTONIC, &ts)) {
+    set_a(context, JS_UNDEFINED);
+    return;
+  }
+  double t = (double)(ts.tv_sec);
+  try_gc(context);
+  set_a(context, double_to_flonum(t));
+}
+
+BUILTIN_FUNCTION(builtin_time_get_msec_monotonic)
+{
+  builtin_prologue();
+  struct timespec ts;
+  if (clock_gettime(CLOCK_REALTIME, &ts)) {
+    set_a(context, JS_UNDEFINED);
+    return;
+  }
+  double t = ((double)ts.tv_sec*1000) + ((double)(ts.tv_nsec)/1000000);
+  try_gc(context);
+  set_a(context, double_to_flonum(t));
+}
+
+BUILTIN_FUNCTION(builtin_time_get_usec_monotonic)
+{
+  builtin_prologue();
+  struct timespec ts;
+  if (clock_gettime(CLOCK_MONOTONIC, &ts)) {
+    set_a(context, JS_UNDEFINED);
+    return;
+  }
+  double t = ((double)ts.tv_sec*1000000) + ((double)(ts.tv_nsec)/1000);
+  try_gc(context);
+  set_a(context, double_to_flonum(t));
+}
+
+BUILTIN_FUNCTION(builtin_time_get_nsec_monotonic)
+{
+  builtin_prologue();
+  struct timespec ts;
+  if (clock_gettime(CLOCK_MONOTONIC, &ts)) {
+    set_a(context, JS_UNDEFINED);
+    return;
+  }
+  double t = ((double)ts.tv_sec*1000000000) + (double)(ts.tv_nsec);
+  try_gc(context);
+  set_a(context, double_to_flonum(t));
+}
+
 ObjBuiltinProp time_funcs[] = {
   { "delay",   builtin_time_delay,  1, ATTR_DE },
   { "udelay",  builtin_time_udelay, 1, ATTR_DE },
+  { "clock",  builtin_time_clock, 0, ATTR_DE },
+  { "get_sec_monotonic",  builtin_time_get_sec_monotonic, 0, ATTR_DE },
+  { "get_msec_monotonic",  builtin_time_get_msec_monotonic, 0, ATTR_DE },
+  { "get_usec_monotonic",  builtin_time_get_usec_monotonic, 0, ATTR_DE },
+  { "get_nsec_monotonic",  builtin_time_get_nsec_monotonic, 0, ATTR_DE },
   { NULL,                    NULL,  0, ATTR_DE }
 };
 
